500b9dfcb59c07914d9148fab525b9e13f245328
suhaneec <suhanichauhan58@gmail.com>
2024-11-14 20:42:34 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+#Title
+st.title("OVERVIEW OF CRIME TRENDS IN INDIA")
+st.header("Get an overview of how crime rates have evolved over time across various states and districts. Identify the most prevalent crime types and the regions with the highest crime rates.")
+
+
